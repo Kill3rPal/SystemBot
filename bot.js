b27e11480f79b84c54f125d33309e69dea0f57cc
Kill3rPal <68537603+Kill3rPal@users.noreply.github.com>
2020-08-06 12:05:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,6 @@ const app = express();
 app.get("/", (request, response) => {
   response.sendStatus(200);
 });
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
 
 ////بكجات
 const { Client, RichEmbed } = require("discord.js");
